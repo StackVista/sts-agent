b0508f664809055192654cf9940ed22d8206ea34
Arnout Engelen <arnouten@bzzt.net>
2016-09-16 10:44:38 +0200

    Fix false positive in sts-check-naming.sh

--- sts-check-naming.sh
+++ sts-check-naming.sh
@@ -3,7 +3,7 @@
 echo "Lines that might need renaming:"
 
 grep -r etc/dd | grep -v README | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*"
-grep -r datadog\\.conf . | grep -v sts-check-naming | grep -v Binary | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*" | grep -v README
+grep -r datadog\\.conf . | grep -v sts-check-naming | grep -v Binary | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*" | grep -v README | grep -v git/logs
 grep -r ddagent.py . | grep -v sts-check-naming | grep -v Binary
 grep -r opt/datadog . | grep -v sts-check-naming
 grep -re "echo.*DataDog" . | grep -v sts-check-naming
