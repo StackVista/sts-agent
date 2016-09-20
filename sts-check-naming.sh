b489e86466491ecd0eca5a2205c2a65e6ef56769
Wytze Hazenberg <wytzehazenberg@gmail.com>
2016-09-20 09:47:31 +0200

    grep fix in renaming check

--- sts-check-naming.sh
+++ sts-check-naming.sh
@@ -2,7 +2,7 @@
 
 echo "Lines that might need renaming:"
 
-grep -r etc/dd | grep -v README | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*"
+grep -r etc/dd . | grep -v README | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*"
 grep -r datadog\\.conf . | grep -v sts-check-naming | grep -v Binary | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*" | grep -v README | grep -v git/logs
 grep -r ddagent.py . | grep -v sts-check-naming | grep -v Binary | grep -v git/logs
 grep -r datadog_agent . | grep -v sts-check-naming | grep -v git/logs
