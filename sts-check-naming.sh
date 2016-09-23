23f79d194edcf061b062be4c53aa3f6c37f3af29
Arnout Engelen <arnouten@bzzt.net>
2016-09-23 10:38:51 +0200

    Don't rename things in the CHANGELOG

--- sts-check-naming.sh
+++ sts-check-naming.sh
@@ -8,7 +8,7 @@ grep -r etc/dd . | grep -v README | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.
 grep -r datadog\\.conf . | grep -v sts-check-naming | grep -v Binary | grep -v CHANGELOG | grep -ve "^[^:]*:\\s*\#.*" | grep -v README | grep -v git/logs
 grep -r ddagent.py . | grep -v sts-check-naming | grep -v Binary | grep -v git/logs
 grep -r datadog_agent . | grep -v sts-check-naming | grep -v git/logs
-grep -r opt/datadog . | grep -v sts-check-naming
+grep -r opt/datadog . | grep -v sts-check-naming | grep -v CHANGELOG
 grep -re "echo.*DataDog" . | grep -v sts-check-naming
 grep -r "from ddagent import" . | grep -v sts-check-naming
 grep -rie "log\.info.*DataDog" . | grep -v sts-check-naming
