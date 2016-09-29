4133b259a5cb862667d45b301203a1ff2a315dc0
ktatarnikov <ktatarnikov@stackstate.com>
2016-09-29 15:15:08 +0200

    Connection Beat integration: correct stackstate.conf location in etc folder

--- connbeat.sh
+++ connbeat.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-STSURL="`grep '^dd_url: ' /opt/stackstate-agent/stackstate.conf | sed 's/^dd_url: //'`" /opt/stackstate-agent/bin/connbeat -c /etc/sts-agent/connbeat.yml -path.logs /var/log/connbeat -path.data /var/lib/connbeat
-
+STSURL="`grep '^dd_url: ' /etc/sts-agent/stackstate.conf | sed 's/^dd_url: //'`" /opt/stackstate-agent/bin/connbeat -c /etc/sts-agent/connbeat.yml -path.logs /var/log/connbeat -path.data /var/lib/connbeat
