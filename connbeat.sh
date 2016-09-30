bc21bec83bbf09aba9a7c93891961d6c0f266b5d
ktatarnikov <ktatarnikov@stackstate.com>
2016-09-30 15:58:37 +0200

    Connection beat integration: changing path.data dir

--- connbeat.sh
+++ connbeat.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-STSURL="`grep '^dd_url: ' /etc/sts-agent/stackstate.conf | sed 's/^dd_url: //'`" /opt/stackstate-agent/bin/connbeat -c /etc/sts-agent/connbeat.yml -path.logs /var/log/stackstate -path.data /var/lib/connbeat
+STSURL="`grep '^dd_url: ' /etc/sts-agent/stackstate.conf | sed 's/^dd_url: //'`" /opt/stackstate-agent/bin/connbeat -c /etc/sts-agent/connbeat.yml -path.logs /var/log/stackstate -path.data /var/lib/stackstate/connbeat
