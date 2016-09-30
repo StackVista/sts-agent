cc013dbbc7403512388e965ed2d25f6b80d993bb
ktatarnikov <ktatarnikov@stackstate.com>
2016-09-30 22:35:32 +0200

    Connection Beat integration: fixing integration issues

--- connbeat.sh
+++ connbeat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export STSURL="`grep '^dd_url: ' /etc/sts-agent/stackstate.conf | sed 's/^dd_url: //'`"
+export APIKEY="`grep '^api_key: ' /etc/sts-agent/stackstate.conf | sed 's/^api_key: //'`"
 
 exec /opt/stackstate-agent/bin/connbeat -c /etc/sts-agent/connbeat.yml -path.logs /var/log/stackstate -path.data /var/lib/stackstate/connbeat
