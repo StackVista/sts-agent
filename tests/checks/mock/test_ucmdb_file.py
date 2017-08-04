2acb2c9d2edd42ad147d4f073c6abbd726013df4
ktatarnikov <ktatarnikov@stackstate.com>
2017-08-04 19:34:56 +0200

    sax style parser for ucmdb tql result

---
+++ tests/checks/mock/test_ucmdb_file.py
@@ -0,0 +1,58 @@
+# stdlib
+import json
+
+from tests.checks.common import AgentCheckTest, Fixtures
+from checks import CheckException
+from utils.ucmdb import UcmdbCIParser
+import itertools
+from unittest import TestCase
+import logging
+
+class TestUcmdbCIParser(TestCase):
+
+    def test_parse_full(self):
+        parser = UcmdbCIParser("tests/checks/fixtures/ucmdb/tql_export_full.xml")
+        parser.parse()
+        components = parser.get_components()
+        relations = parser.get_relations()
+
+        self.assertEquals(len(components), 2)
+        self.assertEquals(components, [
+            {'type': 'business_application',
+             'external_id': 'dab1c91cdc7a6d808b0642cb02ea22f0',
+             'data': {
+                'display_label': 'CRMI (MQCODE)',
+                'name': 'CRMI (MQCODE)',
+                'global_id': 'dab1c91cdc7a6d808b0642cb02ea22f0',
+                'root_class': 'business_application'
+                }
+            },
+            {'type': 'business_application',
+             'data': {'display_label': 'ISSUER LOADBALANCER-SSL-OFFLOADER', 'name': 'ISSUER LOADBALANCER-SSL-OFFLOADER', 'global_id': 'ba21d9dfb1c2ebf4ee951589a3b4ec62', 'root_class': 'business_application'}, 'external_id': 'ba21d9dfb1c2ebf4ee951589a3b4ec62'}])
+        self.assertEquals(len(relations), 1)
+        self.assertEquals(relations, [{
+            'source_id': 'dab1c91cdc7a6d808b0642cb02ea22f0',
+            'type': 'containment',
+            'target_id': '6c01ec45816a40eb866400ff143f4968',
+            'data': {
+                'DiscoveryID2': '6c01ec45816a40eb866400ff143f4968',
+                'DiscoveryID1': 'dab1c91cdc7a6d808b0642cb02ea22f0',
+                'display_label': 'Containment',
+                'end1Id': 'UCMDB%0ARB_BusinessFunction%0A1%0Ainternal_id%3DSTRING%3Ddab1c91cdc7a6d808b0642cb02ea22f0%0A',
+                'end2Id': 'UCMDB%0ARB_BusinessChannel%0A1%0Ainternal_id%3DSTRING%3Dba21d9dfb1c2ebf4ee951589a3b4ec62%0A'
+            },
+            'external_id': 'a9247f4296601c507064ae599bec177e'}])
+
+    def test_parse_minimal(self):
+        self.maxDiff = None
+        parser = UcmdbCIParser("tests/checks/fixtures/ucmdb/tql_export_min.xml")
+        parser.parse()
+        components = parser.get_components()
+        relations = parser.get_relations()
+        self.assertEquals(len(components), 2)
+        self.assertEquals(components, [
+            {'type': 'business_service', 'data': {}, 'external_id': 'dab1c91cdc7a6d808b0642cb02ea22f0'},
+            {'data': {}, 'external_id': 'ba21d9dfb1c2ebf4ee951589a3b4ec62', 'type': 'business_service'}
+            ])
+        self.assertEquals(len(relations), 1)
+        self.assertEquals(relations, [{'source_id': 'dab1c91cdc7a6d808b0642cb02ea22f0', 'type': 'containment', 'target_id': '6c01ec45816a40eb866400ff143f4968', 'data': {'DiscoveryID2': '6c01ec45816a40eb866400ff143f4968', 'DiscoveryID1': 'dab1c91cdc7a6d808b0642cb02ea22f0'}, 'external_id': 'a9247f4296601c507064ae599bec177e'}])
---
+++ utils/ucmdb.py
@@ -0,0 +1,70 @@
+# 3rd party
+import time
+from urllib import quote
+
+from checks.check_status import CheckData
+from checks import AgentCheck, CheckException
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+class UcmdbCIParser:
+    # SAX style parser for UCMDB TQL query result xml
+    #
+    # <object operation="add" name="business_application" ucmdb_id="dab1c91cdc7a6d808b0642cb02ea22f0" id="UCMDB%0Abusiness_application%0A1%0Ainternal_id%3DSTRING%3Ddab1c91cdc7a6d808b0642cb02ea22f0%0A">
+    #   <attribute name="display_label" datatype="STRING">CRMI (MQCODE)</attribute>
+    #   <attribute name="name" datatype="STRING">CRMI (MQCODE)</attribute>
+    #   <attribute name="global_id" datatype="STRING">dab1c91cdc7a6d808b0642cb02ea22f0</attribute>
+    #   <attribute name="root_class" datatype="STRING">business_application</attribute>
+    # </object>
+
+    # <link name="usage" operation="add" ucmdb_id="b5ae9a13b50f4c9cee6851afc085497f">
+    #   <attribute name="DiscoveryID1">35f8a6275733c84fae827b2e7a3998fd</attribute>
+    #   <attribute name="DiscoveryID2">282fbea52182451a03cf7f707ec3a597</attribute>
+    #   <attribute name="end1Id">UCMDB%0ARB_ApplicationService%0A1%0Ainternal_id%3DSTRING%3D35f8a6275733c84fae827b2e7a3998fd%0A</attribute>
+    #   <attribute name="end2Id">UCMDB%0Abusiness_application%0A1%0Ainternal_id%3DSTRING%3D282fbea52182451a03cf7f707ec3a597%0A</attribute>
+    #   <attribute name="display_label" datatype="STRING">Usage</attribute>
+    # </link>
+
+    def __init__(self, file):
+        self.file = file
+        self.components = []
+        self.relations = []
+
+    def parse(self):
+        tree = ET.iterparse(self.file)
+        current_element = dict()
+        current_element['data'] = dict()
+        for event, elem in tree:
+            if event == 'end':
+                if elem.tag == 'attribute':
+                    if 'name' in elem.attrib:
+                        attribute_name = elem.attrib['name']
+                        attribute_value = elem.text
+                        current_element['data'][attribute_name] = attribute_value
+                elif elem.tag == 'object':
+                    if elem.attrib['operation'] == 'add':
+                        current_element['type'] = elem.attrib['name']
+                        current_element['external_id'] = elem.attrib['ucmdb_id']
+                        self.components.append(current_element)
+                    current_element = dict()
+                    current_element['data'] = dict()
+                elif elem.tag == 'link':
+                    if elem.attrib['operation'] == 'add':
+                        current_element['type'] = elem.attrib['name']
+                        current_element['external_id'] = elem.attrib['ucmdb_id']
+                        if 'DiscoveryID1' in current_element['data']:
+                            current_element['source_id'] = current_element['data']['DiscoveryID1']
+                        if 'DiscoveryID2' in current_element['data']:
+                            current_element['target_id'] = current_element['data']['DiscoveryID2']
+                        self.relations.append(current_element)
+                    current_element = dict()
+                    current_element['data'] = dict()
+            elem.clear()
+
+    def get_components(self):
+        return self.components
+
+    def get_relations(self):
+        return self.relations
