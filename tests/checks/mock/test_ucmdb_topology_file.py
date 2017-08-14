720145eaf0405ad8649bfb12c546d2c673a9cc17
ktatarnikov <ktatarnikov@stackstate.com>
2017-08-14 15:09:20 +0200

    covering type fields with tests

--- tests/checks/mock/test_ucmdb_topology_file.py
+++ tests/checks/mock/test_ucmdb_topology_file.py
@@ -33,6 +33,8 @@ class TestUcmdbTopologyFull(AgentCheckTest):
                 {
                     'location': 'tests/core/fixtures/ucmdb/tql_export_full.xml',
                     'tag_attributes': ['root_class','name'],
+                    'component_type_field': 'global_id',
+                    'relation_type_field': 'display_label',
                     'tags': ['mytag']
                 }
             ]
@@ -50,7 +52,7 @@ class TestUcmdbTopologyFull(AgentCheckTest):
             'root_class': 'business_application',
             'tags': ['business_application', 'CRMI (MQCODE)', 'mytag']},
             'externalId': 'dab1c91cdc7a6d808b0642cb02ea22f0',
-            'type': {'name': 'business_application'}})
+            'type': {'name': 'dab1c91cdc7a6d808b0642cb02ea22f0'}})
 
         self.assertEqual(instances[0]['components'][1], {
             'data': {'display_label': 'ISSUER LOADBALANCER-SSL-OFFLOADER',
@@ -59,7 +61,7 @@ class TestUcmdbTopologyFull(AgentCheckTest):
             'root_class': 'business_application',
             'tags': ['business_application','ISSUER LOADBALANCER-SSL-OFFLOADER','mytag']},
             'externalId': 'ba21d9dfb1c2ebf4ee951589a3b4ec62',
-            'type': {'name': 'business_application'}})
+            'type': {'name': 'ba21d9dfb1c2ebf4ee951589a3b4ec62'}})
 
         self.assertEqual(len(instances[0]['relations']), 1)
         self.assertEqual(instances[0]['relations'][0], {'data': {'DiscoveryID1': 'dab1c91cdc7a6d808b0642cb02ea22f0',
@@ -68,10 +70,10 @@ class TestUcmdbTopologyFull(AgentCheckTest):
             'end1Id': 'UCMDB%0ARB_BusinessFunction%0A1%0Ainternal_id%3DSTRING%3Ddab1c91cdc7a6d808b0642cb02ea22f0%0A',
             'end2Id': 'UCMDB%0ARB_BusinessChannel%0A1%0Ainternal_id%3DSTRING%3Dba21d9dfb1c2ebf4ee951589a3b4ec62%0A',
             'tags': ['mytag']},
-            'externalId': 'dab1c91cdc7a6d808b0642cb02ea22f0-containment-6c01ec45816a40eb866400ff143f4968',
+            'externalId': 'dab1c91cdc7a6d808b0642cb02ea22f0-Containment-6c01ec45816a40eb866400ff143f4968',
             'sourceId': 'dab1c91cdc7a6d808b0642cb02ea22f0',
             'targetId': '6c01ec45816a40eb866400ff143f4968',
-            'type': {'name': 'containment'}})
+            'type': {'name': 'Containment'}})
 
 class TestUcmdbTopologyMinimal(AgentCheckTest):
     """
